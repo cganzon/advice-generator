ea757966cee7eab0f6824667ca91354850281fdd
cganzon <crganzon@gmail.com>
2022-02-20 19:38:20 -0800

    created a function to display the advice data when the page first loads and when the button is clicked

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+const adviceId = document.querySelector('.advice-id');
+const adviceText = document.querySelector('.advice-text');
+const adviceBtn = document.querySelector('.advice-btn');
+
+// Function that calls the Advice Slip API to get random advice
 const getRandomAdvice = () => {
     fetch('https://api.adviceslip.com/advice')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayRandomAdvice(data.slip))
     .catch(err => console.log(err));
 };
 
+// Function that displays the advice ID and text on the page
+const displayRandomAdvice = (data) => {
+    adviceId.textContent = `#${data.id}`;
+    adviceText.textContent = `"${data.advice}"`;
+};
+
+// Click event listener on the DOM button
+adviceBtn.addEventListener('click', getRandomAdvice);
+
+// Calling getRandomAdvice to display advice when the page first loads
 getRandomAdvice();
